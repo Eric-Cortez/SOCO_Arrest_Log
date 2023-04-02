e6beb129d2dc89fedb8defbacaf93d49605b4913
Eric-Cortez <ecrcortez@gmail.com>
2023-04-02 08:31:46 -0700

    added footer and link to repo

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+      <footer>
+          <nav>
+              <ul>
+                  <li>
+                      <a href="https://github.com/Eric-Cortez/SOCO_Transparency_Dashboard" target="_blank" rel="noopener noreferrer">GitHub</a>
+                  </li>
+              </ul>
+          </nav>
+      </footer>
+
+  )
+}
