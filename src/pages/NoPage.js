6f1fec0e18b7acd05ef9cc8b6dc628e40db76c70
Eric-Cortez <ecrcortez@gmail.com>
2023-04-04 21:40:34 -0700

    added deployment to workflow

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function NoPage() {
-  return <div className="page-content">
-    <h2 className="pg-header">404 - Not Found</h2>
-    </div>;
+  return (
+    <div className="page-content">
+      <h2 className="pg-header">404 - Not Found</h2>
+    </div>
+  );
 }
