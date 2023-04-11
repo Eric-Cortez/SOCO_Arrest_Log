81cf4cd9ab08ebafb21a04f41712f1f07a1fb055
Eric-Cortez <ecrcortez@gmail.com>
2023-04-10 22:35:30 -0700

    split some code and created fallback spinner

---
+++ src/pages/components/Spinner/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Spinner.css"
+
+export default function index() {
+  return ( 
+    <div className="fallback">Loading...
+      <div className="spinner"></div>
+    </div>
+)
+}
